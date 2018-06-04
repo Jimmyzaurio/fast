aa42e8bff80bb4eac18a99c1ae6335a92c0514e5
Jesus Saucedo Perez <root@Z,vK>
2018-06-04 17:38:57 +0000

    respaldos

---
+++ respaldo/solor2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+USER="rcp"
+PASSWD="rcp"
+
+DIR="/root/server/respaldo/r"
+FILE="startup-config"
+
+ROUTER_IP[0]="192.168.201.15"
+ROUTER_IP[1]="192.168.204.15"
+
+inicial() {
+
+cambia_dir="cd $DIR$1"
+$cambia_dir
+
+ftp -n -v ${ROUTER_IP[$1]} <<END_SCRIPT
+    user $USER $PASSWD
+    put $FILE
+    bye
+END_SCRIPT
+}
+
+inicial 1
+
